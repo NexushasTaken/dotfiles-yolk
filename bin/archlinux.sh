330dd33c4f4ea31d0db5bf47fbb0bf4840c05eaa
NexushasTaken <nexushastaken@gmail.com>
2025-12-07 15:41:13 +0800

    add(bootstrap): add bootstrap for archlinux

---
+++ bin/archlinux.sh
@@ -0,0 +1,19 @@
+source /etc/os-release
+
+function pkg-get() {
+  case "$ID" in
+    arch)
+      sudo pacman -S --needed "$@"
+      ;;
+    *)
+      ;;
+  esac
+}
+
+pkgs=(
+  "bat"
+  "eza"
+  "zoxide"
+)
+
+pkg-get "${pkgs[@]}"
