23156fa92735dc7d02a07f279796cf251b60567f
NexushasTaken <nexushastaken@gmail.com>
2025-12-16 15:15:59 +0800

    add(hyprland): WIP: set cursor variables

--- bin/archlinux.sh
+++ bin/archlinux.sh
@@ -20,6 +20,16 @@ pkgs=(
   # Optionals
   #"github-cli"
   #"superfile"
+
+  # Utilities
+  "wget"
 )
 
 pkg-get "${pkgs[@]}"
+
+if ! compgen -G "$XDG_DATA_HOME/icons/phinger-cursors-*"; then
+  if download https://github.com/phisch/phinger-cursors/releases/latest/download/phinger-cursors-variants.tar.bz2; then
+    run mkdir -p "$XDG_DATA_HOME/icons"
+    run tar xvfj "$CACHE_DIR/phinger-cursors-variants.tar.bz2" -C "$XDG_DATA_HOME/icons"
+  fi
+fi
